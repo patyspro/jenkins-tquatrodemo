c563209c5b9af9aa33179283bf56ab1d33b3c691
patyspro <patyspro@gmail.com>
2021-11-23 14:27:10 -0300

    feature-infra

---
+++ src/main/java/com/infra/jenkinstquatrodemo/JenkinsTquatrodemoApplication.java
@@ -0,0 +1,13 @@
+package com.infra.jenkinstquatrodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTquatrodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTquatrodemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infra/jenkinstquatrodemo/JenkinsTquatrodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infra.jenkinstquatrodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTquatrodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
